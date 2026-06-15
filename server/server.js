062dfc2138596cbea1c339c821e51878fd5f8ffe
Riya Mokale <riyamokale@Riyas-MacBook-Air.local>
2026-06-16 01:14:17 +0530

    server

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..')));
 
 // Submit form
 app.post('/api/submit', (req, res) => {
@@ -65,10 +64,6 @@ res.status(401).json({ success: false, message: "Wrong username or password" });
 }
 }
 );
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
 const PORT = 5000;
